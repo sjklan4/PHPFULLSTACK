0f1354502afd58db116da0254dc2b1313cebce15
sjklan4 <dpeltms4@gmail.com>
2023-04-14 18:09:58 +0900

    0414연습

--- PRACTICE_MIN/board_cnn.php
+++ PRACTICE_MIN/board_cnn.php
@@ -16,7 +16,7 @@ function DB_conn( &$param_conn)
 
     try 
     {
-        $param_conn = new PDO($dns, $user, $host, $pass);
+        $param_conn = new PDO($dns, $user, $pass, $pdo_option);
     } 
     catch (Exception $e) 
     {
@@ -46,8 +46,8 @@ function board_list_fnc(&$param_conn)
 
     $array_prepare =
             array(
-                ":limit"=>$param_conn,
-                ":offset"=>$param_conn
+                ":limit" => $param_conn["limit"],
+                ":offset" => $param_conn["offset"]
             );
 
     $conn = null;
@@ -55,9 +55,43 @@ function board_list_fnc(&$param_conn)
     {
         db_conn( $conn ); //데이터 베이스 연동이 유지가 되게 되면 다른 유저들이 접근을 할 수가 없어서 쿼리 연동시 항상 종료 하는 조건을 붙인다.
         $stmt = $conn->prepare( $sql );
-        $stmt->execute( $arr_prepare );
+        $stmt->execute( $array_prepare );
         $result = $stmt->fetchall();
+    } 
+    catch ( Exception $e) 
+    {
+        return $e->getMessage();
+    }
+    finally //성공여부와 상관없이 null로 커넥션을 초기화 시켜준다.
+    {
+        $conn = null;
+    }
+ //비정상 작동시 try문에서 finally를 실행시키고 catch 문의 값을 리턴시킨다. 그러나 정상 작동시 finally가 작동하고 return을 시킨다. 
+    return $result;        
+}
 
+
+    function select_board_info_cnt()
+    {
+        $sql =  
+        " SELECT "
+        ." count(*) "
+        ." FROM " 
+        ." board_info "
+        ." WHERE "
+        ." board_del_flg = '0' "
+        ;
+        
+        $array_prepare = array();
+
+
+    $conn = null;
+    try 
+    {
+        db_conn( $conn ); //데이터 베이스 연동이 유지가 되게 되면 다른 유저들이 접근을 할 수가 없어서 쿼리 연동시 항상 종료 하는 조건을 붙인다.
+        $stmt = $conn->prepare( $sql );
+        $stmt->execute( $array_prepare );
+        $result = $stmt->fetchall();
     } 
     catch ( Exception $e) 
     {
@@ -71,11 +105,5 @@ function board_list_fnc(&$param_conn)
     return $result;        
 
 }
-
-
-
-
-
-
 
 ?>
